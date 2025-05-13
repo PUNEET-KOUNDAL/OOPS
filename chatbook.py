46d0577c687ef11e857d03c749db10c7e852011b
PUNEET-KOUNDAL <puneetkoundal707@gmail.com>
2025-05-13 20:50:39 +0530

    sign-up sign-in

--- chatbook.py
+++ chatbook.py
@@ -5,6 +5,9 @@ class chatbook :
         self.loggdin = False 
         self.menu()
 
+                
+
+
     def menu(self) :
         user_input = input('''welcome to chatbook !! how you you like to procedd ?
                            1. presee 1 to sing up 
@@ -12,16 +15,42 @@ class chatbook :
                            3. press 3 to write a post 
                            4. press 4 to message a friend 
                            5. press any orhter key to exit \n ''')
-        
+
 
         if user_input == '1' :
-            pass 
+            self.singup() 
         elif user_input == '2' :
-            pass 
+            self.singin()
         elif user_input == '3':
             print('upcoming are in line up')
         else :
             exit()
+
+
+
+    
+    def singup(self):
+        email = input('enter your email -- >  ')
+        password = input('enter you password here -- >  ')
+        self.username =  email
+        self.password = password
+        print('you have singup successfully\n')
+        self.menu()
+
+    def singin(self) :
+        if self.username == ' ' and self.password == ' ' :
+            print("please singup first")
+        else :
+            username = input('enter your usename/email correctly')
+            password = input('enter your password ')
+            if self.username == username and self.password == password :
+                print('thanks for using our services')
+                self.loggdin = True 
+            else :
+                print("please enter the correct crendentials")
+        self.menu()
+
+        
         
 
 
